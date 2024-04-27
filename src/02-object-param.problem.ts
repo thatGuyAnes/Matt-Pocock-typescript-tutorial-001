ba7cd35af8d87e90900eda1d0b28d0ac389065a2
Anes Marzuki <amarzuki@yellowsys.fr>
2024-04-27 10:40:32 +0100

    03"

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+type AddTwoNumbersType = {
+  first: number;
+  second: number;
+};
+
+export const addTwoNumbers = (params: AddTwoNumbersType) => {
   return params.first + params.second;
 };
 
